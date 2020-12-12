f1b2d1f96f717c5245e993d87780044c55040b35
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-12-12 17:23:21 +0400

    create form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Input = ({ label }) => {
+export const Input = ({ label, placeholder }) => {
   return (
     <Container>
       <Label>{label}</Label>
-      <TextInput />
+      <TextInput placeholder={placeholder} autoFocus={false} />
     </Container>
   );
 };
@@ -19,6 +19,16 @@ const Label = styled.p({
   color: '#A4AEB4'
 });
 
-const TextInput = styled.input({
+const TextInput = styled.input`
+  border: 0;
+  border-bottom: 1px solid #D5D9DC;
+  font-size: 18px;
+  font-weight: 600;
+  padding: 3px 8px 8px 0;
 
-})
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid #35A0EE;
+  }
+  
+`;
--- src/pages/ContactDetailsScreen/Form.jsx
+++ src/pages/ContactDetailsScreen/Form.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactTelInput from 'react-telephone-input';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 
 import { Input } from '../../components';
 
 export const Form = () => {
-  return(
+  return (
     <Container>
-      <Input label="Full name" />
+      <Row>
+        <Input label="Full name" placeholder="John Doe" />
+        <ReactTelInput defaultCountry="Azerbaijan" />
+      </Row>
+      <CountryDropdown />
     </Container>
   );
 };
@@ -14,4 +20,11 @@ export const Form = () => {
 const Container = styled.div({
   padding: '10px 270px 0 130px',
   // backgroundColor: 'red'
+});
+
+const Row = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: 20
 });
